0e6873f3daf7c66ea6ba4db4d9f581a671000979
zahidul <codemen.zahidul@gmail.com>
2023-11-12 01:19:49 +0600

    remove unnecessary file

---
+++ FactoryManagement/Infrastructure/Helpers/SeedData.cs
@@ -0,0 +1,67 @@
+﻿using FactoryManagement.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using FactoryManagement.Models;
+
+namespace FactoryManagement.Infrastructure.Helpers
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetService<FactoryManagementEntities>();
+
+            string[] roles = new string[] { "Admin", "Manage", "Operator", "Viewer"};
+
+            foreach (string role in roles)
+            {
+                var roleStore = new RoleStore<IdentityRole>(context);
+
+                if (!context.Roles.Any(r => r.Name == role))
+                {
+                    roleStore.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+
+            var user = new ApplicationUser
+            {
+                
+                Email = "zs.islam93@gmail.com",
+                UserName = "Admin",
+                PhoneNumber = "+111111111111",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString("D")
+            };
+
+
+            if (!context.Users.Any(u => u.UserName == user.UserName))
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var result = userStore.CreateAsync(user);
+
+            }
+
+            AssignRoles(serviceProvider, user.UserName, roles);
+
+            context.SaveChangesAsync();
+        }
+
+        public static async Task<IdentityResult> AssignRoles(IServiceProvider services, string email, string[] roles)
+        {
+            UserManager<ApplicationUser> _userManager = services.GetService<UserManager<ApplicationUser>>();
+            ApplicationUser user = await _userManager.FindByEmailAsync(email);
+            var result = await _userManager.AddToRolesAsync(user.Id, roles);
+
+            return result;
+        }
+
+    }
+}
